bd76c857373a975413a024cb5c685f054f98ad63
cyberspunk <prasadraj954@gmail.com>
2019-11-13 13:37:54 +0530

    Update fb_login.py

--- fb_login.py
+++ fb_login.py
@@ -5,5 +5,5 @@ from selenium import webdriver
 driver=webdriver.Chrome(executable_path="G:\\webdriver\\chromedriver.exe")
 driver.get("https://www.facebook.com")
 driver.find_element_by_name("email").send_keys("prasadraj954@gmail.com")
-driver.find_element_by_name("pass").send_keys("rajprasad123")
+driver.find_element_by_name("pass").send_keys("password") ## type your fb password
 driver.find_element_by_id("loginbutton").click()
